f1b680b5dab2c76d2794229586ed3b5a6c274563
corabrown <coralynnbrown@gmail.com>
2025-05-01 07:58:58 -0500

    handle end of string match

--- app/main.go
+++ app/main.go
@@ -38,73 +38,76 @@ func main() {
 
 func matchLine(line []byte, pattern string) (bool, error) {
 
-	matchFound := false 
+	matchFound := false
 	patternComponents := parsePattern(pattern)
 
-	var matchBeginningOfString bool 
-	if patternComponents[0][0] == '^'{
-		matchBeginningOfString = true 
+	var matchBeginningOfString bool
+	if patternComponents[0][0] == '^' {
+		matchBeginningOfString = true
 		patternComponents = patternComponents[1:]
 	}
+	if patternComponents[len(patternComponents)-1][0] == '$' {
+		patternComponents = patternComponents[:len(patternComponents)-1]
+		startingIndex := len(line) - len(patternComponents)
+		line = line[startingIndex:]
+	}
 
-
-
-	reset := false 
-	matchedPatternIndex := 0 
+	reset := false
+	matchedPatternIndex := 0
 	for _, b := range line {
 		switch string(patternComponents[matchedPatternIndex]) {
 		case "\\d":
-			if (b >= '0' && b <= '9') {
-				matchFound = true 
-				matchedPatternIndex += 1 
-			} else {
-				reset = true 
-			}
-		case "\\w":
-			if (b >= '0' && b <= '9') || (b >= 'A' && b <= 'Z') || (b >= 'a' && b <= 'z') {
-				matchFound = true 
-				matchedPatternIndex += 1 
-			} else {
-				reset = true 
-			}
-		default: 
-			if b == patternComponents[matchedPatternIndex][0] {
-				matchFound = true 
-				matchedPatternIndex += 1 
+			if b >= '0' && b <= '9' {
+				matchFound = true
+				matchedPatternIndex += 1
 			} else {
 				reset = true
 			}
-		}	
-		if (matchedPatternIndex == len(patternComponents)) && matchFound {
-			return true, nil 
+		case "\\w":
+			if (b >= '0' && b <= '9') || (b >= 'A' && b <= 'Z') || (b >= 'a' && b <= 'z') {
+				matchFound = true
+				matchedPatternIndex += 1
+			} else {
+				reset = true
+			}
+		default:
+			if b == patternComponents[matchedPatternIndex][0] {
+				matchFound = true
+				matchedPatternIndex += 1
+			} else {
+				reset = true
+			}
 		}
-		
+		if (matchedPatternIndex == len(patternComponents)) && matchFound {
+			return true, nil
+		}
+
 		if reset {
 			if matchBeginningOfString {
-				return false, nil 
+				return false, nil
 			}
 			matchFound = false
-			matchedPatternIndex = 0 
-			reset = false 
+			matchedPatternIndex = 0
+			reset = false
 		}
 	}
 
 	if strings.HasPrefix(pattern, "[^") && strings.HasSuffix(pattern, "]") {
 		for _, b := range line {
 			if !bytes.ContainsAny([]byte{b}, pattern[2:len(pattern)-1]) {
-				return true, nil 
+				return true, nil
 			}
 		}
-		return false, nil 
+		return false, nil
 	}
 	if strings.HasPrefix(pattern, "[") && strings.HasSuffix(pattern, "]") {
-		for _, r := range pattern[1:len(pattern)-1] {
+		for _, r := range pattern[1 : len(pattern)-1] {
 			if ok := bytes.ContainsAny(line, string(r)); ok {
-				return ok, nil 
+				return ok, nil
 			}
 		}
 	}
-	
+
 	return false, nil
 }
 
@@ -114,7 +117,7 @@ func parsePattern(pattern string) [][]byte {
 	for i := range pattern {
 		if pattern[i] == byte('\\') {
 			currentCharacter = "\\"
-			continue 
+			continue
 		}
 		if currentCharacter != "" {
 			if (pattern[i] == 'd') || (pattern[i] == 'w') {
@@ -127,5 +130,5 @@ func parsePattern(pattern string) [][]byte {
 			output = append(output, []byte{pattern[i]})
 		}
 	}
-	return output 
+	return output
 }
